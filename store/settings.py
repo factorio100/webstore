b203d0011028f6d1dbc4cae2f0ef5d9869e79d41
cherif <cherif44@outlook.fr>
2025-10-16 18:46:29 +0100

    debugx2

--- store/settings.py
+++ store/settings.py
@@ -87,6 +87,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # debug
+     'django.middleware.http.ConditionalGetMiddleware',
 ]
 
 ROOT_URLCONF = 'store.urls'
