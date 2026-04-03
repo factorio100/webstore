7ff6b84eb42eb3289626fa67f835244eb2a180b8
cherif <cherif44@outlook.fr>
2026-04-03 12:58:14 +0100

    removed debug false

--- store/settings.py
+++ store/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = False
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
